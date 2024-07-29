71f817c9b565efd85b23476b9c2276a6a6e0ef7e
edison_ <benjhamin.edison@gmail.com>
2024-07-28 22:35:46 -0500

    nuevo menu rol

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -50,6 +50,18 @@
                         Usuarios
                     </button>
                 </RouterLink>
+                <RouterLink :to="{ name: 'Roles' }">
+                    <button
+                        class="inline-flex relative items-center py-[10px] px-[20px] w-full text-sm font-medium hover:bg-gray-700 active:bg-gray-900 focus:bg-gray-600 hover:text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
+                        </svg>
+
+                        ´Roles
+                    </button>
+                </RouterLink>
                 <RouterLink :to="{ name: 'Bicicletas' }" class="button">
                     <button
                         class="inline-flex relative items-center py-[10px] px-[20px] w-full text-sm font-medium hover:bg-gray-700 active:bg-gray-900 focus:bg-gray-600 hover:text-white">
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Auth from "@/components/AuthPage.vue";
 import Session from "@/components/SessionPage.vue";
-import UsuariosGestor from "@/pages/UsuarioPages.vue";
-import BicicletasGestor from "@/pages/BicicletasPages.vue";
-import EstacionesGestor from "@/pages/EstacionesPages.vue";
-import LoginComponent from "@/pages/LoginPages.vue";
-import AlquilerGestor from "@/pages/AlquilerPages.vue";
-import MantenimientoGestor from "@/pages/MantenimeintoPages.vue";
-import ViajesGestor from "@/pages/ViajesPages.vue";
-import UbicacionComponent from "@/pages/UbicacionPages.vue";
-import RegistroComponent from "@/pages/RegistroPages.vue";
-import HomeComponent from "@/pages/HomePages.vue";
+import UsuariosPages from "@/pages/UsuarioPages.vue";
+import RolesPages from "@/pages/RolesPages.vue";
+import BicicletasPages from "@/pages/BicicletasPages.vue";
+import EstacionesPages from "@/pages/EstacionesPages.vue";
+import LoginPages from "@/pages/LoginPages.vue";
+import AlquilerPages from "@/pages/AlquilerPages.vue";
+import MantenimientoPages from "@/pages/MantenimeintoPages.vue";
+import ViajesPages from "@/pages/ViajesPages.vue";
+import UbicacionPages from "@/pages/UbicacionPages.vue";
+import RegistroPages from "@/pages/RegistroPages.vue";
+import HomePages from "@/pages/HomePages.vue";
 
 
 const routes = [
@@ -19,14 +20,15 @@ const routes = [
     component: Session,
     path: '/',
     children: [
-      { name: 'Home', path: '/', component: HomeComponent },
-      { name: 'Usuarios', path: '/Usuarios', component: UsuariosGestor },
-      { name: 'Bicicletas', path: '/Bicicletas', component: BicicletasGestor },
-      { name: 'Estaciones', path: '/Estaciones', component: EstacionesGestor },
-      { name: 'Mantenimientos', path: '/Mantenimientos', component: MantenimientoGestor },
-      { name: 'Alquileres', path: '/Alquileres', component: AlquilerGestor },
-      { name: 'Viajes', path: '/Viajes', component: ViajesGestor },
-      { name: 'Ubicaciones', path: '/Ubicaciones', component: UbicacionComponent }
+      { name: 'Home', path: '/', component: HomePages},
+      { name: 'Usuarios', path: '/Usuarios', component: UsuariosPages},
+      { name: 'Roles', path: '/Roles', component: RolesPages},
+      { name: 'Bicicletas', path: '/Bicicletas', component: BicicletasPages},
+      { name: 'Estaciones', path: '/Estaciones', component: EstacionesPages},
+      { name: 'Mantenimientos', path: '/Mantenimientos', component: MantenimientoPages },
+      { name: 'Alquileres', path: '/Alquileres', component: AlquilerPages },
+      { name: 'Viajes', path: '/Viajes', component: ViajesPages },
+      { name: 'Ubicaciones', path: '/Ubicaciones', component: UbicacionPages }
     ]
   },
   {
@@ -34,8 +36,8 @@ const routes = [
     component: Auth,
     path: '/',
     children: [
-      { name: 'Registro', path: '/Registro', component: RegistroComponent },
-      { name: 'Login', path: '/Login', component: LoginComponent }
+      { name: 'Registro', path: '/Registro', component: RegistroPages },
+      { name: 'Login', path: '/Login', component: LoginPages }
     ]
   },
   
